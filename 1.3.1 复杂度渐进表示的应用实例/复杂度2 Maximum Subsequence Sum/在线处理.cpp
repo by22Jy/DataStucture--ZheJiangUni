f8bacd7c71812ba8c3526951139670cc6e5f4868
SuperHack0328 <1730495747@qq.com>
2023-11-05 00:28:50 +0800

    通过在线处理的做法解决 时间复杂度o(n)

--- 1.3.1 复杂度渐进表示的应用实例/复杂度2 Maximum Subsequence Sum/在线处理.cpp
+++ 1.3.1 复杂度渐进表示的应用实例/复杂度2 Maximum Subsequence Sum/在线处理.cpp
@@ -2,46 +2,45 @@
 // sample换1个数字。有正负，负数开头结尾，有并列最大和	
 // 全是负数的情况
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 const int N = 10010;
 int a[N];
 
-void solve(int a[],int n){
-    // 全是负数的情况
-    bool isAllNegative = true;
-    for(int i=0;i<n;i++) {
-        if(a[i] > 0) {
-            isAllNegative = false;
-            break;
-        }    
-    }
-    if(isAllNegative){
-        printf("0 0 0\n");
-        return;
-    }
-    
-    int ThisSum = 0,MaxSum = 0;
-    int StartNum = 0,EndNum = 0;
-    for(int i=0;i<n;i++){
+void solve(int a[], int n) {
+    // 让MaxSum=-1 而不是0 这样可以处理所有值都是负数和都是0的情况
+    // 多开一个tempstart变量 只有最大值更新时 才让start变量指向tempstart
+    int ThisSum = 0, MaxSum = -1;
+    int StartNum = 0, EndNum = 0;
+    int tempStartNum = 0;
+
+    for (int i = 0; i < n; i++) {
         ThisSum += a[i];
-        if(ThisSum > MaxSum) {
+        if (ThisSum > MaxSum) {
             MaxSum = ThisSum;
+            StartNum = tempStartNum;
             EndNum = i;
         }
-        else if(ThisSum < 0) {
+        else if (ThisSum < 0) {
             ThisSum = 0;
-            StartNum = i+1;
+            tempStartNum = i + 1;
         }
     }
-    printf("%d %d %d\n",MaxSum,a[StartNum],a[EndNum]);
+
+    if (MaxSum < 0) {
+        // 全是负数的情况
+        MaxSum = 0;
+        StartNum = 0;
+        EndNum = n - 1;
+    }
+
+    cout << MaxSum << " " << a[StartNum] << " " << a[EndNum] << endl;
 }
 
-int main()
-{
-
+int main() {
     int k;
-    scanf("%d",&k);
-    for(int i=0;i<k;i++) scanf("%d",&a[i]);
-    solve(a,k);
+    cin >> k;
+    for (int i = 0; i < k; i++) cin >> a[i];
+    solve(a, k);
+    return 0;
 }
